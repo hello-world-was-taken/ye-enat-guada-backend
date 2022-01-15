4a25827ad17737fbbcdcdeaccdee79632f7f78db
hello_world <haileformal@gmail.com>
2022-01-15 16:49:24 +0300

    add serializers for all models

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,23 @@
+from rest_framework import serializers
+from .models import Vendor, Customer
+from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+      model = User
+      fields = ('id', 'username', 'password')
+
+class VendorSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=True)
+    class Meta:
+      model = Vendor
+      fields = ('username', 'phone_number', 'rating', 'long', 'lat')
+
+class CustomerSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=True)
+    class Meta:
+      model = Customer
+      fields = ('username', 'phone_number', 'rating')
       
 # class ArtistSerializer(serialisers.ModelSerializer):
 #   songs = SongSerializer(many=True)
