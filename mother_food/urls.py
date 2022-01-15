19c63c9d39c56dfd7c9e4cd012bcb91ff4143d6a
hello_world <haileformal@gmail.com>
2022-01-15 21:13:51 +0300

    fix image upload bug setting add MEDIA_ROOT path

--- mother_food/urls.py
+++ mother_food/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from users.views import UserUploadedPicture
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('<str:username>/<str:password>', views.fun),
     path('reg/', UserUploadedPicture.as_view())
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
